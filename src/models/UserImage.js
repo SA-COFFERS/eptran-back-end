63b50779cb465472139867481392fdac7b7cec17
Vitor Rafael Marcelino <111241590+vitorrmarcelino@users.noreply.github.com>
2024-06-07 09:20:21 -0300

    update in user image model

--- src/models/UserImage.js
+++ src/models/UserImage.js
@@ -3,14 +3,21 @@ const { Model, DataTypes } = require('sequelize');
 class UserImage extends Model {
   static init(sequelize) {
     super.init({
+      user_image_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       user_image_path: DataTypes.STRING,
     }, {
       sequelize,
+      tableName: 'user_image',
     });
   }
 
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_image_fk_user' });
+    this.hasOne(models.User, { foreignKey: 'user_fk_image_path' });
   }
 }
 
